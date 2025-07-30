f5545f11e11ab81a92744736d172df7435359443
abdrahman100 <hosniabdrahman60@gmail.com>
2025-07-30 07:17:39 +0100

    first add

---
+++ scrept.js
@@ -0,0 +1,20 @@
+const cartItem = document.querySelectorAll("#cartItem");
+
+cartItem.forEach((cart) => {
+  const cartItem = cart.querySelector("#cartItem");
+  const button = cart.querySelector("#addTo");
+  const message = cart.querySelector("#messageadd");
+  button.addEventListener("click", () => {
+    message.style = "display: block";
+    cart.style.background = "#d4edda";
+    cart.style.transition =
+      "background .3s ease-in-out ,  display .3s ease-in-out,transform .3s ease-in-out , box-shadow .3s ease-in-out";
+
+    setTimeout(() => {
+      cart.style.background = "#ffffffff";
+      cart.style.transition =
+        "background .3s ease-in-out , display .3s ease-in-out,transform .3s ease-in-out ,box-shadow .3s ease-in-out";
+    }, 1000);
+  });
+});
+console.log(cartItem);
